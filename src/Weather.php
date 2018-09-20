03258c793f8f2147e47f5d39fd408e7fab6bf9ac
刘海朋 <991695066@qq.com>
2018-09-20 20:13:44 +0800

    Weather

---
+++ src/Weather.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Lhp9916\Weather;
+
+
+use GuzzleHttp\Client;
+
+class Weather
+{
+    protected $key;
+    protected $guzzleOptions = [];
+
+    public function __construct(string $key)
+    {
+        $this->key = $key;
+    }
+
+    public function getHttpClient()
+    {
+        return new Client($this->guzzleOptions);
+    }
+
+    public function setGuzzleOptions(array $options)
+    {
+        $this->guzzleOptions = $options;
+    }
+
+    public function getWeather($city, string $type = 'base', string $format = 'json')
+    {
+        //todo
+    }
+}
